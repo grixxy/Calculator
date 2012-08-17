c40eb85498a085aeec0a2d8fc127c7ee2d6edfa7
Gregory <gregory@Gregorys-MacBook-Pro-2.local>
2012-08-16 18:07:34 -0700

    Initial Commit

---
+++ Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Gregory on 8/16/12.
+//  Copyright (c) 2012 Gregory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Gregory on 8/16/12.
+//  Copyright (c) 2012 Gregory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
