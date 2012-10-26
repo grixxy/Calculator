760a1423f4a1d6562560f4be2c647746827be5fd
Gregory Shukhter <grixxy@gmail.com>
2012-10-26 08:50:52 -0700

    seconds commit

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Gregory on 8/24/12.
+//  Copyright (c) 2012 Gregory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+@end
---
+++ Calculator/CalculatorBrain.m
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by Gregory on 8/24/12.
+//  Copyright (c) 2012 Gregory. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+@end
